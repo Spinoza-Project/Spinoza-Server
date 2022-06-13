e6ee6af85bc654f5934d4b9e0deab6d0b4167275
HyeokJoon Kong <orijoon98@gmail.com>
2022-06-13 19:54:41 +0900

    [FEAT] Feed timestamp 추가

--- src/models/Feed.ts
+++ src/models/Feed.ts
@@ -1,27 +1,32 @@
 import mongoose from "mongoose";
 import { FeedInfo } from "../interfaces/feed/FeedInfo";
 
-const FeedSchema = new mongoose.Schema({
-    plantId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "Plant",
-    },
-    farmerId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "Farmer",
-    },
-    images: [
-        {
+const FeedSchema = new mongoose.Schema(
+    {
+        plantId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "Plant",
+        },
+        farmerId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "Farmer",
+        },
+        images: [
+            {
+                type: String,
+                required: true,
+            },
+        ],
+        content: {
             type: String,
             required: true,
         },
-    ],
-    content: {
-        type: String,
-        required: true,
     },
-});
+    {
+        timestamps: true,
+    }
+);
 
 export default mongoose.model<FeedInfo & mongoose.Document>("Feed", FeedSchema);
